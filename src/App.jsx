22ee77d133f09ba1f30a351d675d37bbc888e918
Vishal <vishal@gmail.com>
2023-08-01 21:23:54 +0530

    React Calculator

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import './App.css'
+import React, { useState } from 'react'
+
+function App() {
+  const [input, setInput] = useState('')
+  const handleButtonClick = (value) => {
+    if (value == 'C') {
+      setInput('')
+    } else if (value == '<') {
+      setInput(input.slice(0, -1))
+    } else if (value == '=') {
+      try {
+        setInput(eval(input).toString())
+      }
+      catch (err) {
+        setInput('Invalid')
+      }
+    }
+    else {
+      setInput((prev) => prev + value)
+    }
+  }
+  return (
+    <div className='container'>
+      <div className="calc">
+        <h1 id='input'>{input}</h1>
+        <div>
+          <button onClick={() => handleButtonClick('C')}>C</button>
+          <button onClick={() => handleButtonClick('<')}>&lt;</button>
+          <button onClick={() => handleButtonClick('%')}>%</button>
+          <button onClick={() => handleButtonClick('/')}>/</button>
+        </div>
+        <div>
+          <button onClick={() => handleButtonClick('7')}>7</button>
+          <button onClick={() => handleButtonClick('8')}>8</button>
+          <button onClick={() => handleButtonClick('9')}>9</button>
+          <button onClick={() => handleButtonClick('*')}>*</button>
+        </div>
+        <div>
+          <button onClick={() => handleButtonClick('4')}>4</button>
+          <button onClick={() => handleButtonClick('5')}>5</button>
+          <button onClick={() => handleButtonClick('6')}>6</button>
+          <button onClick={() => handleButtonClick('-')}>-</button>
+        </div>
+        <div>
+          <button onClick={() => handleButtonClick('1')}>1</button>
+          <button onClick={() => handleButtonClick('2')}>2</button>
+          <button onClick={() => handleButtonClick('3')}>3</button>
+          <button onClick={() => handleButtonClick('+')}>+</button>
+        </div>
+        <div>
+          <button onClick={() => handleButtonClick('0')}>0</button>
+          <button onClick={() => handleButtonClick('00')}>00</button>
+          <button onClick={() => handleButtonClick('.')}>.</button>
+          <button onClick={() => handleButtonClick('=')}>=</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
